8da37ba3d677c79540a22ee36cda1f40188a0ccb
Repurica <65117070+Repurica@users.noreply.github.com>
2023-08-23 09:57:43 +0800

    S2 SCAM

--- main_bot_function.py
+++ main_bot_function.py
@@ -5,6 +5,7 @@ from telegram.ext import ConversationHandler,ApplicationBuilder, ContextTypes, C
 import pathlib
 import os
 import shutil
+import requests
 
 
 #log
@@ -120,6 +121,66 @@ async def send_file_without_upload_cmd(update: Update, context: ContextTypes.DEF
         text="plz use /upload to start an upload"
     )
 
+
+
+async def idea(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    context.user_data["next_offset"]=0
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="the next message will be used to query for articles!"
+    )
+    return query
+
+
+
+async def query(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if("query" not in context.user_data):
+        context.user_data["query"]=update.message.text
+
+    URL ="http://api.semanticscholar.org/graph/v1/paper/search"
+    # offset: skip first 10 result, limit: limit the number of records output, fields
+    PARAMS = {'query':context.user_data["query"],"offset":context.user_data["next_offset"],"fields":"title,authors"}
+    r=requests.get(url=URL, params=PARAMS)
+    data=r.json()
+    output=""
+    print(data["data"])
+    for paper in data["data"]:
+        output+="<b>"+paper["title"]+"</b>\n\nPaper ID: "+paper["paperId"]+"\n\nAuthors:\n"
+
+        for author in paper["authors"]:
+            output+=author["name"]+"\n"
+        output+="\n\n"
+
+    output+="here are "+str(context.user_data["next_offset"]+1)+" - "+str(context.user_data["next_offset"]+10)+" records, /next for the next 10, /query_finish to stop"
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=output,
+        parse_mode="HTML"
+    )
+    return next
+
+async def next(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["next_offset"]+=10
+    await query(update, context)
+
+
+
+async def query_finish(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    del context.user_data["next_offset"]
+    del context.user_data["query"]
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="ok here are your files, lmk if it works"
+    )
+    return ConversationHandler.END
+
+
+
+
+
+
 #wrap up
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
@@ -136,16 +197,18 @@ if __name__ == '__main__':
     #load bot handler 
 
 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    application.add_handler(echo_handler)
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
 
-    # send file without /upload will not be processed
 
-
+    query_handler=ConversationHandler(
+        entry_points=[CommandHandler('idea', idea)],
+        states={query:[MessageHandler(filters.TEXT & (~filters.COMMAND), query)],
+                next:[CommandHandler("next", next)]},
+        fallbacks=[CommandHandler('query_finish', query_finish)])
+    application.add_handler(query_handler)
  
     # use /upload to start an upload, then upload file as you like, 
     # choose /cancel to remove folders,
@@ -160,7 +223,8 @@ if __name__ == '__main__':
     send_file_without_upload_cmd_handler=MessageHandler(filters.Document.ALL, send_file_without_upload_cmd)
     application.add_handler(send_file_without_upload_cmd_handler)
 
-
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
     # must come after everything
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
